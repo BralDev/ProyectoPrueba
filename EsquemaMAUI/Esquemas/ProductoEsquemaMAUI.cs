d198b8de5d2f61123cddb7121b6c8397be83097c
BralDev <jesusbrayanjoaquin@gmail.com>
2026-03-06 11:46:34 -0500

    Añade proyecto EsquemaMAUI con modelos de productos
    Se incorpora el nuevo proyecto EsquemaMAUI (.NET Framework 4.8) a la solución, incluyendo modelos de datos para operaciones CRUD de productos y el archivo AssemblyInfo.cs. Se actualizan las configuraciones de la solución y se corrige el encabezado de AppProductos.csproj para evitar problemas de codificación.

---
+++ EsquemaMAUI/Esquemas/ProductoEsquemaMAUI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsquemaMAUI.Esquemas
+{
+    public class StatusBase
+    {
+        public int pnCodigo { get; set; }
+        public string pcMensaje { get; set; }
+    }
+  
+    public class ProductoListaModel
+    {
+        public int pnIdePro { get; set; }
+        public string pcNomPro { get; set; }
+        public string pcDesPro { get; set; }
+        public decimal pnPrePro { get; set; }
+        public int pnStoPro { get; set; }
+        public int pnIdeSed { get; set; }
+        public DateTime ptFecPro { get; set; }
+    }
+
+    public class ProductosListRPT : StatusBase
+    {
+        public ProductoListaModel[] paProductos { get; set; }
+    }
+    
+    public class ProductoCrearRQT
+    {
+        public string pcNomPro { get; set; }
+        public string pcDesPro { get; set; }
+        public decimal pnPrePro { get; set; }
+        public int pnStoPro { get; set; }
+        public int pnIdeSed { get; set; }
+    }
+
+    public class ProductoCrearRPT : StatusBase
+    {
+        public int pnIdePro { get; set; }
+        public string pcNomPro { get; set; }
+        public string pcDesPro { get; set; }
+        public decimal pnPrePro { get; set; }
+        public int pnStoPro { get; set; }
+        public int pnIdeSed { get; set; }
+        public DateTime ptFecPro { get; set; }
+    }
+    
+    public class ProductoActualizarRQT
+    {
+        public int pnIdePro { get; set; }
+        public string pcNomPro { get; set; }
+        public string pcDesPro { get; set; }
+        public decimal pnPrePro { get; set; }
+        public int pnStoPro { get; set; }
+        public int pnIdeSed { get; set; }
+    }
+
+    public class ProductoActualizarRPT : StatusBase
+    {
+        public int pnIdePro { get; set; }
+        public string pcNomPro { get; set; }
+        public string pcDesPro { get; set; }
+        public decimal pnPrePro { get; set; }
+        public int pnStoPro { get; set; }
+        public int pnIdeSed { get; set; }
+        public DateTime ptFecPro { get; set; }
+    }
+    
+    public class ProductoEliminarRQT
+    {
+        public int pnIdePro { get; set; }
+    }
+
+    public class ProductoEliminarRPT : StatusBase
+    {
+        public int pnIdePro { get; set; }
+    }
+}
